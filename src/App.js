07fcc81c47dc251d1d7980b3169185e30e6d16b5
Kinrezc <matt.czernik@gmail.com>
2019-12-09 18:31:30 -0500

    Add custom credential support

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Web3ReactProvider } from "@web3-react/core";
 import { TerminalHttpProvider } from "@terminal-packages/sdk";
 import Web3 from "web3";
 
-import TestComponent from "./components/TestComponent";
-
-const getLibrary = provider => {
-  const library = new Web3(
-    new TerminalHttpProvider({
-      customHttpProvider: provider,
-      apiKey: "IYFLu2akdq6D4WhIqhZVVw==",
-      projectId: "ryAqzgjMRZoJWnxv",
-      source: "MetaMask"
-    })
-  );
-  console.log(library);
-  library.pollingInterval = 8000;
-  return library;
-};
+import MainContent from "./components/main-content/MainContent";
 
 const App = () => {
+  const [values, setValues] = useState({
+    apiKey: "IYFLu2akdq6D4WhIqhZVVw==",
+    projectId: "ryAqzgjMRZoJWnxv"
+  });
+
+  const getLibrary = provider => {
+    console.log(provider);
+    const library = new Web3(
+      new TerminalHttpProvider({
+        customHttpProvider: provider,
+        apiKey: values.apiKey,
+        projectId: values.projectId,
+        source: "MetaMask"
+      })
+    );
+    library.pollingInterval = 8000;
+    return library;
+  };
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <TestComponent />
+      <MainContent setValues={setValues} values={values} />
     </Web3ReactProvider>
   );
 };
---
+++ src/components/constants.js
@@ -0,0 +1,24 @@
+import {
+  injected,
+  portis,
+  fortmatic,
+  network,
+  torus,
+  walletconnect
+} from "../connectors";
+
+export const connectorTypes = {
+  Injected: injected,
+  Portis: portis,
+  Fortmatic: fortmatic,
+  Network: network,
+  Torus: torus,
+  WalletConnect: walletconnect
+};
+
+export const SOCIAL_LINKS = {
+  TWITTER: "https://twitter.com/terminaldotco",
+  GITHUB: "https://github.com/Terminal-Systems",
+  MEDIUM: "https://medium.com/terminaldotco",
+  TERMINAL: "https://terminal.co/"
+};
